4a2cf0b6d87d2384bd4360070d2ea0a335c93d38
Nika <nika@Nikas-iMac.local>
2020-11-07 01:22:50 +0400

    ft

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// import the React and ReactDOm libraries
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+// npm start --reset-cahce
+
+// Create a react component
+const App = () => {
+    const x = 5;
+return (
+    <div>
+        
+    </div>
+);
+};
+
+//Take the react component and show it on the screen
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+
+);
